be46e4edc3bf71d5fc2976f8bb2f9327379cc2b7
Jon Tyler <k98kurz@gmail.com>
2021-08-22 12:12:08 -0400

    added classifier strings

--- setup.py
+++ setup.py
@@ -20,4 +20,9 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=requirements,
     python_requires='>=3.6.0',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Security :: Cryptography",
+        "License :: OSI Approved :: ISC License (ISCL)",
+    ],
 )
