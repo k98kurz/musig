ff9ba5e15fa1d7fc13655b93c6e0c9d8b03e6d8f
Jon Tyler <k98kurz@gmail.com>
2021-08-22 12:08:15 -0400

    init

---
+++ readme.md
@@ -0,0 +1,16 @@
+# MuSig
+
+This is a simple-to-use implementation of the MuSig protocol, ported over from
+the yet-unreleased [Pycelium-SDK](https://github.com/k98kurz/pycelium-sdk)
+project (once the basics are finished, that project will be opened to the
+public). This exists separately as a means to refactor the draft version of the
+MuSig code and make it available to devs for experimentation earlier than the
+rest of the SDK.
+
+# Installation
+
+Installation instructions will be here once the package is published to pypi.
+
+# Usage
+
+Usage instructions will be here once the refactoring is complete.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('readme.md', 'r') as f:
+    readme = f.read()
+
+with open('license.txt', 'r') as f:
+    license = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+
+setup(
+    name='MuSig',
+    version='0.0.1',
+    author='k98kurz@github',
+    url='https://github.com/k98kurz/musig',
+    description='Simple-to-use package implementing a MuSig multi-sig protocol.',
+    long_description=readme,
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=requirements,
+    python_requires='>=3.6.0',
+)
