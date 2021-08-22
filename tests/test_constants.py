e28ffe13faca0920851c15c92bff399bfdca8ebc
Jon Tyler <k98kurz@gmail.com>
2021-08-22 12:40:12 -0400

    short test suite for constants

---
+++ tests/test_constants.py
@@ -0,0 +1,23 @@
+from context import musig
+from nacl.signing import SigningKey
+import unittest
+
+
+class TestMuSigConstants(unittest.TestCase):
+    """Test suite for the constants used by the various classes."""
+
+    def test_musig_has_MAX_WAIT_TIME_FOR_COMMITMENTS(self):
+        assert hasattr(musig, 'MAX_WAIT_TIME_FOR_COMMITMENTS')
+        assert type(musig.MAX_WAIT_TIME_FOR_COMMITMENTS) is int
+
+    def test_musig_has_MAX_WAIT_TIME_FOR_PUBLIC_NONCES(self):
+        assert hasattr(musig, 'MAX_WAIT_TIME_FOR_PUBLIC_NONCES')
+        assert type(musig.MAX_WAIT_TIME_FOR_PUBLIC_NONCES) is int
+
+    def test_musig_has_MAX_WAIT_TIME_FOR_PARTIAL_SIGS(self):
+        assert hasattr(musig, 'MAX_WAIT_TIME_FOR_PARTIAL_SIGS')
+        assert type(musig.MAX_WAIT_TIME_FOR_PARTIAL_SIGS) is int
+
+
+if __name__ == '__main__':
+    unittest.main()
