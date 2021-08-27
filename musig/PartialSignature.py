b4f1a70c45d09ef2b5726126d30359a81364d9a8
Jon Tyler <k98kurz@gmail.com>
2021-08-26 20:06:46 -0400

    PartialSignature refactor

--- musig/PartialSignature.py
+++ musig/PartialSignature.py
@@ -7,130 +7,56 @@ from nacl.signing import SigningKey
 import nacl.bindings
 
 
-class PartialSignature(dict, AbstractPartialSignature):
+class PartialSignature(AbstractPartialSignature):
     """A class that handles creation, serialization, and deserialization of
         partial signatures used to create MuSig aggregate signatures.
     """
-    def __init__(self, data=None) -> None:
-        """Initialize an instance with params for the `create` method or
-            deserialize.
-            Call with `data=(skey: SigningKey, r: bytes, L: bytes, X: PublicKey,
-            R: bytes, M: bytes)` to create a new PartialSignature.
-            Call with `data=str` or `data=bytes` to implicitly call `deserialize`.
-            Call with `data=dict` to instantiate with key:value definition, where
-            each value is b64 encoded.
+    def __init__(self, data: dict = None) -> None:
+        """Initialize an instance with dict.
+            At a minimum data should include c_i and s_i keys with base64-encoded
+            values of 32 bytes each; key of R with base64-encoded value of 32
+            bytes is optional; key of M with base64-encoded value of bytes is
+            also optional.
         """
         if data is None:
             raise ValueError('cannot instantiate PartialSignature with None')
 
-        if type(data) in [list, tuple] and len(data) == 6:
-            data = self.create(*data)
+        if not isinstance(data, dict):
+            raise TypeError('data must be type dict')
 
-        if type(data) in (bytes, str):
-            data = self.deserialize(data)
-
-        if isinstance(data, dict):
-            super().__init__(data)
-            if 'c_i' in data:
-                self._c_i = b64decode(data['c_i'])
-            if 's_i' in data:
-                self._s_i = b64decode(data['s_i'])
-            if 'R' in data:
-                self._R = b64decode(data['R'])
-            if 'M' in data:
-                self._M = b64decode(data['M'])
-
-    def __str__(self) -> str:
-        """Result of calling str() on an instance."""
-        parts = [
-            self.c_i.hex(),
-            self.s_i.hex(),
-            self.R.hex(),
-            self.M.hex(),
-        ]
-        return '16.' + '.'.join(parts)
-
-    def __repr__(self) -> str:
-        """Result of calling repr() on an instance."""
-        parts = [
-            b64encode(self.c_i).decode(),
-            b64encode(self.s_i).decode(),
-            b64encode(self.R).decode(),
-            b64encode(self.M).decode(),
-        ]
-        return '64.' + '.'.join(parts)
+        if 'c_i' in data:
+            self.c_i = b64decode(data['c_i'])
+        if 's_i' in data:
+            self.s_i = b64decode(data['s_i'])
+        if 'R' in data:
+            self.R = b64decode(data['R'])
+        if 'M' in data:
+            self.M = b64decode(data['M'])
 
     def __bytes__(self) -> bytes:
         """Result of calling bytes() on an instance."""
         return self.c_i + self.s_i + self.R + self.M
 
-    def __hash__(self) -> int:
-        """Make class hashable for inclusion in sets."""
-        return hash(self.c_i + self.s_i + self.R + self.M)
-
-    def __eq__(self, other) -> bool:
-        """Enforce timing-attack safe comparison."""
-        if not isinstance(other, self.__class__):
-            return False
-        return bytes_are_same(bytes(self), bytes(other))
-
-    def serialize(self) -> str:
-        """Return a serialized representation of the instance."""
-        return repr(self)
-
     @classmethod
-    def deserialize(cls, data) -> PartialSignature:
-        """Deserialize some `data` into an instance.
-            Acceptable types are `str` and `bytes`.
-            Acceptable formats for `str` are '16.{hexadecimal.parts}',
-            '64.{base64.parts}', and 'json.{json_string}'.
-            Parts for a hex or b64 encoded serialization are c_i, s_i,
-            R, and M delimited by '.'.
-            Acceptable format for `bytes` is c_i+s_i+R+M, where
-            c_i, s_i, and R each has len=32.
-        """
-        if type(data) is bytes:
-            if len(data) < 32*3+1:
-                raise ValueError('bytes must be at least 97 length')
-            c_i = data[:32]
-            s_i = data[32:64]
-            R = data[64:96]
-            M = data[96:]
-            return cls({
-                'c_i': b64encode(c_i).decode(),
-                's_i': b64encode(s_i).decode(),
-                'R': b64encode(R).decode(),
-                'M': b64encode(M).decode(),
-            })
-        elif type(data) is str:
-            # split into parts
-            parts = data.split('.')
-            if parts[0] != 'json' and len(parts) != 5:
-                raise ValueError('input str must have 5 parts delimited by .')
-            if parts[0] == '16':
-                c_i = bytes.fromhex(parts[1])
-                s_i = bytes.fromhex(parts[2])
-                R = bytes.fromhex(parts[3])
-                M = bytes.fromhex(parts[4])
-                return cls({
-                    'c_i': b64encode(c_i).decode(),
-                    's_i': b64encode(s_i).decode(),
-                    'R': b64encode(R).decode(),
-                    'M': b64encode(M).decode(),
-                })
-            elif parts[0] == '64':
-                return cls({
-                    'c_i': parts[1],
-                    's_i': parts[2],
-                    'R': parts[3],
-                    'M': parts[4],
-                })
-            elif parts[0] == 'json':
-                return cls(loads('.'.join(parts[1:])))
-            else:
-                raise ValueError('unknown/invalid serialization')
-        else:
-            raise ValueError('unknown/invalid serialization')
+    def from_bytes(cls, data: bytes) -> PartialSignature:
+        if type(data) is not bytes:
+            raise TypeError('data must be bytes of len == 64 or >=96')
+
+        if len(data) < 64 or (len(data) > 64 and len(data) < 96):
+            raise ValueError('data must be bytes of len == 64 or >=96')
+
+        new_data = {
+            'c_i': b64encode(data[:32]).decode(),
+            's_i': b64encode(data[32:64]).decode()
+        }
+
+        if len(data) >= 96:
+            new_data['R'] = b64encode(data[64:96]).decode()
+
+        if len(data) > 96:
+            new_data['M'] = b64encode(data[96:]).decode()
+
+        return cls(new_data)
 
     @classmethod
     def create(cls, skey: SigningKey, r: bytes, L: bytes, X: AbstractPublicKey,
@@ -150,7 +76,52 @@ class PartialSignature(dict, AbstractPartialSignature):
             'M': b64encode(M).decode(),
         })
 
-    c_i = property(lambda self: self._c_i if hasattr(self, '_c_i') else None)
-    s_i = property(lambda self: self._s_i if hasattr(self, '_s_i') else None)
-    R = property(lambda self: self._R if hasattr(self, '_R') else None)
-    M = property(lambda self: self._M if hasattr(self, '_R') else None)
+    @property
+    def c_i(self):
+        return self._c_i if hasattr(self, '_c_i') else None
+
+    @c_i.setter
+    def c_i(self, data: bytes):
+        if type(data) is not bytes:
+            raise TypeError('data must be bytes of len 32')
+        if len(data) != 32:
+            raise ValueError('data must be bytes of len 32')
+
+        self['c_i'] = data
+
+    @property
+    def s_i(self):
+        return self._s_i if hasattr(self, '_s_i') else None
+
+    @s_i.setter
+    def s_i(self, data: bytes):
+        if type(data) is not bytes:
+            raise TypeError('data must be bytes of len 32')
+        if len(data) != 32:
+            raise ValueError('data must be bytes of len 32')
+
+        self['s_i'] = data
+
+    @property
+    def R(self):
+        return self._R if hasattr(self, '_R') else None
+
+    @R.setter
+    def R(self, data: bytes):
+        if type(data) is not bytes:
+            raise TypeError('data must be bytes of len 32')
+        if len(data) != 32:
+            raise ValueError('data must be bytes of len 32')
+
+        self['R'] = data
+
+    @property
+    def M(self):
+        return self._M if hasattr(self, '_M') else None
+
+    @M.setter
+    def M(self, data: bytes):
+        if type(data) is not bytes:
+            raise TypeError('data must be bytes')
+
+        self['M'] = data
--- tests/test_PartialSignature.py
+++ tests/test_PartialSignature.py
@@ -22,11 +22,17 @@ class TestMuSigPartialSignature(unittest.TestCase):
         assert inspect.isclass(musig.PartialSignature)
 
     def test_PartialSignature_init_raises_ValueError_when_called_without_param(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as err:
             musig.PartialSignature()
+        assert str(err.exception) == 'cannot instantiate PartialSignature with None'
+
+    def test_PartialSignature_init_raises_TypeError_when_called_with_invalid_input(self):
+        with self.assertRaises(TypeError) as err:
+            musig.PartialSignature('not a dict')
+        assert str(err.exception) == 'data must be type dict'
 
     def test_PartialSignature_instances_have_correct_attributes(self):
-        pkey = musig.PublicKey(self.verify_keys)
+        pkey = musig.PublicKey.create(self.verify_keys)
         n = musig.Nonce()
         psig = musig.PartialSignature.create(self.signing_keys[0], n.r, pkey.L,
             pkey, n.R, b'hello world')
@@ -35,57 +41,32 @@ class TestMuSigPartialSignature(unittest.TestCase):
         assert hasattr(psig, 'R') and type(psig.R) is bytes
         assert hasattr(psig, 'M') and type(psig.M) is bytes
 
-    def test_PartialSignature_deserialize_raises_ValueError_when_given_invalid_serialization(self):
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize(b'invalid bytes')
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize('invalid str')
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize('invalid.str')
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize([])
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize(('',''))
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize({'a','b'})
-        with self.assertRaises(ValueError):
-            musig.PartialSignature.deserialize({'a':'b'})
+    def test_PartialSignature_from_bytes_raises_ValueError_or_TypeError_when_given_invalid_serialization(self):
+        with self.assertRaises(TypeError) as err:
+            musig.PartialSignature.from_bytes('not bytes')
+        assert str(err.exception) == 'data must be bytes of len == 64 or >=96'
+        with self.assertRaises(ValueError) as err:
+            musig.PartialSignature.from_bytes(b'invalid bytes')
+        assert str(err.exception) == 'data must be bytes of len == 64 or >=96'
 
     def test_PartialSignature_instances_serialize_and_deserialize_properly(self):
-        pkey = musig.PublicKey(self.verify_keys)
+        pkey = musig.PublicKey.create(self.verify_keys[0:1])
         n = musig.Nonce()
+        M = b'hello world'
         ps0 = musig.PartialSignature.create(self.signing_keys[0], n.r, pkey.L,
-            pkey, n.R, b'hello world')
+            pkey, n.R, M)
         str1 = str(ps0)
-        str2 = repr(ps0)
+        bts = bytes(ps0)
         js = dumps(ps0)
-        ps1 = musig.PartialSignature(str1)
-        ps2 = musig.PartialSignature(str2)
+        ps1 = musig.PartialSignature.from_str(str1)
+        ps2 = musig.PartialSignature.from_bytes(bts)
         ps3 = musig.PartialSignature(loads(js))
-        ps4 = musig.PartialSignature('json.' + js)
 
-        assert type(str1) is str and str1[:2] == '16'
-        assert type(str2) is str and str2[:2] == '64'
+        assert type(str1) is str and len(str1) == 96*2 + len(M)*2
         assert type(js) is str
         assert ps0 == ps1
         assert ps1 == ps2
         assert ps2 == ps3
-        assert ps3 == ps4
-
-    def test_PartialSignature_instances_can_be_members_of_sets(self):
-        pkey = musig.PublicKey(self.verify_keys)
-        n1, n2, n3 = musig.Nonce(), musig.Nonce(), musig.Nonce()
-        ps1 = musig.PartialSignature.create(self.signing_keys[0], n1.r, pkey.L,
-            pkey, n1.R, b'hello world')
-        ps2 = musig.PartialSignature.create(self.signing_keys[1], n2.r, pkey.L,
-            pkey, n2.R, b'hello world')
-        ps3 = musig.PartialSignature.create(self.signing_keys[2], n3.r, pkey.L,
-            pkey, n3.R, b'hello world')
-        ps33 = musig.PartialSignature(str(ps3))
-
-        pses = set([ps1, ps2, ps3, ps33])
-        assert len(pses) == 3
-
 
 if __name__ == '__main__':
     unittest.main()
